55dbfb4fd7184875c6c51a1cd0cbd255ef5554e1
Manoj18121812 <gavhaneam@gmail.com>
2024-09-11 23:09:36 +0530

    Add manoj

---
+++ problemcompany/Manoj.java
@@ -0,0 +1,13 @@
+package practi;
+
+public class Manoj {
+
+	public static void main(String[] args) {
+		int a=5;
+		int b=a++ + --a + ++a + --a + --a + a++ + a++;
+		System.out.println(a);
+		System.out.println(b);
+
+	}
+
+}
